a0f30034b62606e19e41a013a464b9d824b66635
Iaa <102237135+Iaaaaaaa@users.noreply.github.com>
2025-04-14 21:40:50 +0800

    pls

---
+++ api/index.php
@@ -0,0 +1,17 @@
+<?php
+
+// Vercel will run this file as a serverless function.
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
